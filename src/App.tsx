b046d00a43cbba7d099f9d2c77e41880f506f972
daibogh <daibogh@gmail.com>
2021-05-03 17:40:34 +0300

    feat: added list and deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import {ItemsList} from './ItemsList';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ItemsList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ItemsList/ItemsList.tsx
@@ -0,0 +1,2 @@
+const ItemsList = () => <div />;
+export default ItemsList;
