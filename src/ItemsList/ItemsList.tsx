5a9adc89314c55ff99f6eaeed20d84e76d1f9dfa
daibogh <daibogh@gmail.com>
2021-05-08 19:46:02 +0300

    feat (redux-long-op): added long op

--- src/ItemsList/ItemsList.tsx
+++ src/ItemsList/ItemsList.tsx
@@ -3,6 +3,7 @@ import {useAppDispatch, useAppSelector} from '../hooks';
 import io, {Socket} from 'socket.io-client';
 import {
   setList,
+  someLongOp,
   startFetching,
   stopFetching,
   updateList,
@@ -13,22 +14,41 @@ const ItemsList: FC = () => {
   const isActive = useAppSelector(store => store.list.isActiveConnection);
   const dispatch = useAppDispatch();
   const socketRef = useRef<Socket | null>(null);
+  const legacyCounterRef = useRef<any>(null);
   useEffect(() => {
     if (isActive) {
+      legacyCounterRef.current = setInterval(() => dispatch(someLongOp()), 500);
       socketRef.current = io('http://localhost:5000');
       socketRef.current.on('list:value', value => {
-        dispatch(setList(value as number[]));
+        dispatch(
+          setList(
+            value.map((item: number) => ({
+              backgroundColor: `rgb(${Math.floor(
+                Math.random() * 255,
+              )},${Math.floor(Math.random() * 255)},${Math.floor(
+                Math.random() * 255,
+              )})`,
+              width: item,
+            })),
+          ),
+        );
       });
       socketRef.current.on('list:update', value => {
-        dispatch(updateList(value as [number, number]));
+        dispatch(updateList(value as [number]));
         performance.mark('list:update--start');
       });
       socketRef.current.emit('list:get');
-    } else if (!!socketRef.current) {
-      socketRef.current.disconnect();
-      // console.log(performance.getEntriesByName('list:update'));
+    } else {
+      if (!!legacyCounterRef.current) {
+        clearInterval(legacyCounterRef.current);
+      }
+      if (!!socketRef.current) {
+        socketRef.current.disconnect();
+        // console.log(performance.getEntriesByName('list:update'));
+      }
     }
     return () => {
+      clearInterval(legacyCounterRef.current);
       socketRef.current?.disconnect();
     };
   }, [isActive, dispatch]);
@@ -40,6 +60,9 @@ const ItemsList: FC = () => {
         'list:update--start',
         'list:update--end',
       );
+      if (items.every(elem => elem.width >= 100)) {
+        dispatch(stopFetching());
+      }
     } catch (e) {
       console.log(e);
     }
@@ -48,7 +71,13 @@ const ItemsList: FC = () => {
     <div>
       <ul>
         {items.map((item, idx) => (
-          <li key={idx}>{item}</li>
+          <li
+            key={idx}
+            style={{
+              background: item.backgroundColor,
+              width: item.width,
+            }}
+          />
         ))}
       </ul>
       <button
--- src/store/slices/listSlice.ts
+++ src/store/slices/listSlice.ts
@@ -3,22 +3,26 @@ import {createSlice} from '@reduxjs/toolkit';
 export const listSlice = createSlice({
   name: 'list',
   initialState: {
-    value: [] as number[],
+    value: [] as {width: number; backgroundColor: string}[],
     isActiveConnection: false,
+    legacyList: Array.from({length: 999999}, (_, i) => i + 1),
   },
   reducers: {
     set: (state, action) => {
       state.value = action.payload;
     },
-    update: (state, action: {payload: [number, number]}) => {
-      const [index, value] = action.payload;
-      state.value[index] = value;
+    update: (state, action: {payload: [number]}) => {
+      const [index] = action.payload;
+      if (state.value[index].width < 100) state.value[index].width += 5;
     },
     stop: state => {
       state.isActiveConnection = false;
     },
     start: state => {
       state.isActiveConnection = true;
+    },
+    someLongOp: state => {
+      state.legacyList.push(state.legacyList.length);
     },
   },
 });
@@ -29,6 +33,7 @@ export const {
   update: updateList,
   stop: stopFetching,
   start: startFetching,
+  someLongOp,
 } = listSlice.actions;
 
 export default listSlice.reducer;
