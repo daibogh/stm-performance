93d262ce22e2526fdba116e0117e26333b4f43f0
daibogh <DaiBogh@gmail.com>
2021-05-09 20:03:49 +0300

    fix: chart

--- src/hooks/usePerformanceMeasure.ts
+++ src/hooks/usePerformanceMeasure.ts
@@ -1,5 +1,6 @@
 import {useCallback} from 'react';
 type StartMarkFunction = () => void;
+type EndMarkFunction = () => void;
 type CollectPerformanceFunction = () => void;
 export const usePerformanceMeasure = ({
   startMark,
@@ -11,19 +12,21 @@ export const usePerformanceMeasure = ({
   measureMark: string;
 }): {
   startMark: StartMarkFunction;
+  endMark: EndMarkFunction;
   collectPerformanceList: CollectPerformanceFunction;
 } => {
-  performance.mark(endMark);
-  try {
-    performance.measure(measureMark, startMark, endMark);
-  } catch (e) {
-    console.log(e);
-  }
-
   return {
     startMark: useCallback(() => {
       performance.mark(startMark);
     }, [startMark]),
+    endMark: useCallback(() => {
+      performance.mark(endMark);
+      try {
+        performance.measure(measureMark, startMark, endMark);
+      } catch (e) {
+        console.log(e);
+      }
+    }, [endMark, measureMark, startMark]),
     collectPerformanceList: useCallback(
       () => performance.getEntriesByName(measureMark),
       [measureMark],
