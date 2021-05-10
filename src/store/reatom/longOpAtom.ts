d47f01e4ff0226b61acc051729bd569950ebb0eb
daibogh <DaiBogh@gmail.com>
2021-05-10 05:25:44 +0300

    feat: added initial state for long op atom

--- src/store/reatom/longOpAtom.ts
+++ src/store/reatom/longOpAtom.ts
@@ -7,6 +7,8 @@ import {
 } from '@reatom/core';
 
 export const longOpAction = declareAction('longOpAction');
-export const longOpAtom = declareAtom<number[]>('listAtom', [], on => [
-  on(longOpAction, state => [...state, state.length]),
-]);
+export const longOpAtom = declareAtom<number[]>(
+  'listAtom',
+  Array.from({length: 999999}, (_, i) => i + 1),
+  on => [on(longOpAction, state => [...state, state.length])],
+);
